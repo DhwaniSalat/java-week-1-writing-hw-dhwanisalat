9d21a1e3bf5489bf981f4b72be796bc5cd8c6b80
DhwaniSalat <dhwanisalat72@gmail.com>
2024-04-24 16:11:57 +0100

    java week1 writting is done

---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,23 @@
+package homework_week_5;
+/**
+ * Write a programme that prints your BIO Data.
+ */
+
+public class Programme_1_BioData {
+
+    public static void main(String[] args) {
+        System.out.println("            BIO DATA           ");
+        System.out.println("Name             : Jane Smite");
+        System.out.println("Mobile           : 07912345678");
+        System.out.println("Gender           : Female");
+        System.out.println("Date of Brith    : 01.05.2000");
+        System.out.println("Marital Status   : Single");
+        System.out.println("Languages Known  : English, Spanish and German");
+        System.out.println("Qualification    : Software Tester");
+        System.out.println("Experience       : Agile Tester with 5 years of experience");
+        System.out.println("Address          : 123 Abc Street, London AB1 2CD");
+        System.out.println("Place            : London");
+        System.out.println("Date             : 02.10.2021");
+        System.out.println("Signature        : JSmith");
+    }
+}
